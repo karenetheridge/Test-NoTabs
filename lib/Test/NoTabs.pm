7fa0a3fb1b78f2adc8cc0ced3e55435f414d1468
Tomas Doran <bobtfish@bobtfish.net>
2012-06-25 21:21:26 +0100

    Version 1.3

--- lib/Test/NoTabs.pm
+++ lib/Test/NoTabs.pm
@@ -10,7 +10,7 @@ use File::Find;
 
 use vars qw( $VERSION $PERL $UNTAINT_PATTERN $PERL_PATTERN);
 
-$VERSION = '1.2';
+$VERSION = '1.3';
 
 $PERL    = $^X || 'perl';
 $UNTAINT_PATTERN  = qr|^([-+@\w./:\\]+)$|;
