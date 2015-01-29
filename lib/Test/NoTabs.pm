fff9aa308e57126b526b0a682ec796992423f23b
Tomas Doran <tdoran@yelp.com>
2015-01-29 10:32:02 +0000

    Bump version and add changelog

--- lib/Test/NoTabs.pm
+++ lib/Test/NoTabs.pm
@@ -10,7 +10,7 @@ use File::Find;
 
 use vars qw( $VERSION $PERL $UNTAINT_PATTERN $PERL_PATTERN);
 
-$VERSION = '1.3';
+$VERSION = '1.4';
 
 $PERL    = $^X || 'perl';
 $UNTAINT_PATTERN  = qr|^([-+@\w./:\\]+)$|;
